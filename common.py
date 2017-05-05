e92e2b7c96769d9eef50326b59454fb6d7469379
Jim Lynch <jim@jam.sessionsnet.org>
2017-05-04 23:01:08 -0700

    add parse_spell_string(), which reads a list of chords and adds them to a dict

--- common.py
+++ common.py
@@ -10,3 +10,36 @@ seventh_voicing_list = []
 big_four_list = []
 
 chord_spellings = {}
+
+# func parse_spell_string
+# parses spelling string into (chordname, chord spelling) pairs,
+# then adds them to output dict with chordname as the key
+#  takes:
+#   spelling String
+#   output dict
+#   optionally, chord type string (like m, 7, 7sys, m7)
+#  returns: output dict
+
+def parse_spell_string(*args):
+    if len(args) < 2:
+        print("parse_spell_string: too few args")
+        return {}
+    elif len(args) > 3:
+        print("parse_spell_string: too many args")
+        return {}
+    else:
+        spell_str = args[0]
+        out_dict = args[1]
+
+        ch_type = ""
+        if len(args) == 3:
+            ch_type = args[2]
+
+        spell_list = spell_str.split()
+        for ch_sp in spell_list:
+            spelling = ch_sp.split(",")
+            root = spelling[0]
+
+            out_dict[root + ch_type] = spelling
+
+        return out_dict
