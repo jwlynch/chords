3bf37c5b39ecb08271cfdbd5038a71787b78b7b3
Jim Lynch <jim@jam.sessionsnet.org>
2017-05-05 03:08:28 -0700

    add a comment to chords/common.py documenting the chord spelling system

--- common.py
+++ common.py
@@ -9,6 +9,25 @@ seventh_voicing_list = []
 
 big_four_list = []
 
+# Chord spellings: is a dict whose keys are chord names,
+# and whose values are the lists of notes in those chords.
+#
+# the possible triad chord types are:
+#  "" (e.g., nothing, for example, C, Eb): Major Triad
+#  "m" (e.g., Cm, Dbm): Minor Triad
+#  "dim" (e.g., Abdim): Diminished Triad
+#  "#5" (e.g., G#5): Augmented Triad
+#  "sus" (e.g., Fsus): Major Triad with a Suspended Fourth
+#
+# the possible 7th chord types are:
+#  "maj7" (e.g., Cmaj7): Major seventh_list
+#  "maj7sus" (e.g., F#maj7sus): Major Seventh Chord with a Suspended Fourth
+#  "7" (e.g., Ab7): Dominant Seventh Chord
+#  "7sus" (e.g., Bb7sus) Dominant Seventh CHord with a Suspended Fourth
+#  "m7" (e.g., Em7): Minor Seventh Chord
+#  "m7b5" (e.g., Bm7b5): Minor Seventh Chord with a Flatted Fifth
+#  "dim7" (e.g., Adim7): Fully Diminished Seventh Chord
+
 chord_spellings = {}
 
 # func parse_spell_string
